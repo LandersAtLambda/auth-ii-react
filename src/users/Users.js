5ac0f2db73ea65388b9203c0b1270b8db35f6932
Mike Landers <landers.mike@gmail.com>
2019-02-28 16:18:30 -0800

    added filter of users and some style

--- src/users/Users.js
+++ src/users/Users.js
@@ -4,24 +4,56 @@ import axios from 'axios';
 
 function Users() {
 	const [users, setUsers] = useState([]);
+	const [filtered, setFiltered] = useState(null);
+	const [dept, setDept] = useState([]);
 
 	useEffect(() => {
 		axios
 			.get('/users')
 			.then(res => {
 				setUsers(res.data.users);
+				setDept(res.data.depts);
 			})
 			.catch(err => console.log(err.message));
 	}, []);
 
+	const filterDept = (e, name) => {
+		e.preventDefault();
+		let filter = users.filter(user => user.department.includes(name));
+		setFiltered(filter);
+	};
+
 	return (
-		<div className="userList">
-			{users.map(user => (
-				<div className="user" key={user.id}>
-					<h3>Username: {user.username}</h3>
-					<p>Department: {user.department}</p>
-				</div>
-			))}
+		<div className="userWrapper">
+			<div className="sidebar">
+				<button
+					onClick={() => {
+						setFiltered(null);
+					}}>
+					All
+				</button>
+				{dept.map((dept, index) => (
+					<button key={index} onClick={e => filterDept(e, dept.department)}>
+						{dept.department}
+					</button>
+				))}
+			</div>
+
+			<div className="userList">
+				{filtered
+					? filtered.map(user => (
+							<div className="user" key={user.id}>
+								<h3>Username: {user.username}</h3>
+								<p>Department: {user.department}</p>
+							</div>
+					  ))
+					: users.map(user => (
+							<div className="user" key={user.id}>
+								<h3>Username: {user.username}</h3>
+								<p>Department: {user.department}</p>
+							</div>
+					  ))}
+			</div>
 		</div>
 	);
 }
